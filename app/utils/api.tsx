c5071c35dae5ea61a51a4d217708de23c8cb7145
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2023-05-17 13:16:13 +0530

    type fixes

--- app/utils/api.tsx
+++ app/utils/api.tsx
@@ -1,61 +1,36 @@
 import axios from "axios";
 import { auth } from "./firebase";
-import { setPersistence,browserSessionPersistence } from "firebase/auth";
+import { setPersistence, browserSessionPersistence } from "firebase/auth";
 import sessionManagerWithoutFirebase from "./sessionManager";
-import { onAuthStateChanged } from "firebase/auth";
-import { useState,useEffect } from "react";
-
+import { onAuthStateChanged, User } from "firebase/auth";
+import { useState, useEffect } from "react";
 
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-const storeAuthentication = (token, user) => {
-    sessionManagerWithoutFirebase.setUser(user);
+
+const storeAuthentication = (token: string, user: User) => {
+  sessionManagerWithoutFirebase.setUser(user);
   localStorage.setItem("token", token);
 };
 
-    export const handleLogin = async (username, password) => {
-
-      try {
-    // await setPersistence(auth, browserSessionPersistence); // Set session persistence
-
-
-const response = await axios.post(`${baseUrl}/login`, {
-  username,
-  password,
-}, {
-  withCredentials: true,
-});
-
-
-console.log("NEW LOGIN REQUEST")
-        const token = response.data.token;
-        console.log("Token:", token);
-        console.log(response.data);
-        const { id, role,firstName, mail } = response.data;
-    const user = { id, role, firstName, username };
-  console.log("response::" , response)
-  console.log("user::" , response.data.user)
-  if (response) { 
-console.log("handle")
-        storeAuthentication(response.data.token, response.data.user);
+export const handleLogin = async (username: string, password: string) => {
+  try {
+    const response = await axios.post(
+      `${baseUrl}/login`,
+      {
+        username,
+        password,
+      },
+      {
+        withCredentials: true,
       }
+    );
 
+    const { id, role, firstName, mail } = response.data;
+    const user = { id, role, firstName, username };
 
-  // const cookieOptions = {
-  //       httpOnly: true, // Only accessible by the server
-  //       secure: true, // Only sent over HTTPS
-  //       maxAge: 30 * 1000, // Expiration time in milliseconds (30 seconds)
-  //       sameSite: 'none', // Allow cross-site requests
-  //     };
-
-  //     document.cookie = `token=${token}; ${Object.entries(cookieOptions).map(([key, value]) => `${key}=${value}`).join('; ')}`;
-
-
-    console.log("id:", response.data.id);
-    // if (response.data.role === "admin") {
-    //   window.location.href = '/admin'; // Redirect to admin page for admin users
-    // } else {
-    //   console.log("normal user");
-    // }
+    if (response) {
+      storeAuthentication(response.data.token, response.data.user);
+    }
 
     return response.data;
   } catch (error) {
@@ -64,66 +39,41 @@ console.log("handle")
   }
 };
 
-
-
-
-export const handleSignup = async (fname, lname, email, password) => {
+export const handleSignup = async (
+  fname: string,
+  lname: string,
+  email: string,
+  password: string
+) => {
   try {
-    // await setPersistence(auth, browserSessionPersistence);
-
-
-
     const response = await axios.post(`${baseUrl}/signup`, {
       fname,
       lname,
       email,
       password,
     });
+
     console.log("Login successful", response);
-
-
-
-    // window.location.href = `${baseUrl}/`;
     console.error("Registered ");
-    return response.data; // Return the response data if needed
+    return response.data;
   } catch (error) {
-    console.error("Signup failed", error.response);
-    throw error; // Throw the error to be handled by the caller
+    console.error("Signup failed");
+    throw error;
   }
 };
 
-// You can define other functions for different API requests, such as POST, PUT, DELETE, etc.
 
-
-const handlecustomLoginAuthentication = async (user) => {
-  if (user) {
-    // User is signed in
-  //  const token = 
-  // const { id, role, firstName, username } = user;
-    console.log("User signed in now handle custom authentication:", user.role);
-    // Perform actions for logged in user, e.g., show user-specific content
-    // Set state or update UI to reflect logged in state
-    // Example: setLoggedIn(true);
-  } else {
-    // User is signed out
-    console.log("User signed out");
-
-    // Perform actions for logged out user, e.g., show login form
-    // Set state or update UI to reflect logged out state
-    // Example: setLoggedIn(false);
-  }
-};
 export const customSignOutUser = async () => {
   try {
     localStorage.removeItem("user");
     localStorage.removeItem("token");
-    handlecustomLoginAuthentication(null);
+    
   } catch (error) {
     console.error("Sign out failed", error);
   }
 };
 
-const CustomSessionManager = ({ updateUser }) => {
+const CustomSessionManager = ({ updateUser }: { updateUser: (user: User | null) => void }) => {
   useEffect(() => {
     const userString = localStorage.getItem("user");
     const user = userString ? JSON.parse(userString) : null;
@@ -146,10 +96,3 @@ const CustomSessionManager = ({ updateUser }) => {
 };
 
 export default CustomSessionManager;
-// export const auth = getAuth();
-// signOut(auth).then(() => {
-//  console.log("Logout successful");
-// }).catch((error) => {
-//   console.error("Logout failed", error);
-//     throw error;
-// });
