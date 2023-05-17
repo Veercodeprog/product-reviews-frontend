dc349b5bf5f5567e99e605f1a9a2cb3572f038d5
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2023-05-17 13:49:19 +0530

    type fixes

--- app/utils/sessionManager.tsx
+++ app/utils/sessionManager.tsx
@@ -4,11 +4,18 @@ import axios from "axios";
 import { auth } from "./firebase";
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 // SessionManager.js
+type UserType = {
+  id: string;
+  name: string;
+  firstName: string;
+role: string;
+  // ... other properties
+};  
 
 class sessionManagerWithoutFirebase {
   // Other code...
 
-  static setUser(user) {
+  static setUser(user: UserType) {
     localStorage.setItem("user", JSON.stringify(user));
   }
   static getUser() {
