c909011236a4d2b224708badf74324aa575fd1b7
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2023-05-18 23:54:46 +0530

    design fixes

--- app/components/homepage/home-button-list.tsx
+++ app/components/homepage/home-button-list.tsx
@@ -1,6 +1,6 @@
 export default function HomeButtonList() {
 return(
- <div className="-ml-0  mr-5 sm:ml-0 flex  mb-3">
+ <div className="-ml-0  mx-5 sm:ml-0 flex  mb-3">
                 <button
                 type="button"
                 className="home-btn  bg-white text-gray-700 border border-gray-300 rounded-full text-sm sm:px-3 mr-2 mb-2  sm:h-11 flex-shrink-0"
